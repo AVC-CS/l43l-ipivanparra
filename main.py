17212ef0ccd0201d8fcb0a01ef184eb32b0b3e65
Ivan <parraivan127@gmail.com>
2024-10-06 22:13:49 -0700

    Commit 4

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def main():
     total = 0
 
+    ## Lab 4-4
     numbers = [0] * 5
     for i in range(len(numbers)):
         numbers[i] = int(input('Enter a value: '))
@@ -8,13 +9,9 @@ def main():
     print(numbers)
     for v in numbers:
         total += v
-
-    
-
     
     print(total)
 
-    
     return total, numbers
 
 
